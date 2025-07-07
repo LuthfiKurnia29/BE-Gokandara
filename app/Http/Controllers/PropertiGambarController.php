bb75f78bfe7aabd25dda97679b0ca6b22594fc9f
DESKTOP-65JDH9G\lkurn <luthfi.sekawanmedia@gmail.com>
2025-07-07 20:04:09 +0700

    init some table for project

---
+++ app/Http/Controllers/PropertiGambarController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Properti_Gambar;
+use Illuminate\Http\Request;
+
+class PropertiGambarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Properti_Gambar $properti_Gambar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Properti_Gambar $properti_Gambar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Properti_Gambar $properti_Gambar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Properti_Gambar $properti_Gambar)
+    {
+        //
+    }
+}
