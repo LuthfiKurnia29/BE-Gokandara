a3d446f359920d2ab38126ff439e8ca012f39d0f
DESKTOP-65JDH9G\lkurn <luthfi.sekawanmedia@gmail.com>
2025-07-20 18:35:01 +0700

    fixing relation /me

--- app/Models/UserMenuAccess.php
+++ app/Models/UserMenuAccess.php
@@ -8,4 +8,10 @@ class UserMenuAccess extends Model
 {
     //
     protected $guarded = ['id'];
+    public function menu(){
+        return $this->belongsTo(Menu::class, 'menu_id');
+    }
+    public function user_role(){
+        return $this->belongsTo(UserRole::class, 'user_role_id');
+    }
 }
