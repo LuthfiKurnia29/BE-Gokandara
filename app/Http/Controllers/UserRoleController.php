acc7183c15c88451c21c98c1e63e08d146f3688c
DESKTOP-65JDH9G\lkurn <luthfi.sekawanmedia@gmail.com>
2025-07-20 08:29:36 +0700

    api user role & unit

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Http\Request;
 
 class UserRoleController extends Controller
 {
     //
+    public function getRoleByUserId(string $id)
+    {
+        $user = User::where('id', $id)->first();
+        if($user != null){
+            $userRole = UserRole::where('user_id', $user->id)->get();
+            return response()->json([
+                'status' => true,
+                'message' => "Role of this user has successfully get",
+                'data' => $userRole
+            ]);
+        }else{
+            return response("Error to get data", 400);
+        }
+    }
 }
