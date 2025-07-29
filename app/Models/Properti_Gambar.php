72a2ef3d22483a5285ef874120d8f128d2d10a21
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-07-29 14:42:45 +0700

    Update Properti_Gambar.php

--- app/Models/Properti_Gambar.php
+++ app/Models/Properti_Gambar.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Properti_Gambar extends Model
 {
     protected $guarded = ['id'];
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute() {
+        return asset('storage/' . $this->image);
+    }
 }
