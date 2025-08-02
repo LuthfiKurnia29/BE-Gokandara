1df104ca9105e19e4faed36596f6b90f30276518
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-02 18:28:19 +0700

    Update MenuAccessSeeder.php

--- database/seeders/MenuAccessSeeder.php
+++ database/seeders/MenuAccessSeeder.php
@@ -52,6 +52,11 @@ class MenuAccessSeeder extends Seeder {
             'menu_id' => 8,
             'is_allowed' => true
         ]);
+        UserMenuAccess::create([
+            'user_role_id' => 1,
+            'menu_id' => 9,
+            'is_allowed' => true
+        ]);
 
         UserMenuAccess::create([
             'user_role_id' => 2,
@@ -121,6 +126,42 @@ class MenuAccessSeeder extends Seeder {
         ]);
         UserMenuAccess::create([
             'user_role_id' => 3,
+            'menu_id' => 7,
+            'is_allowed' => true
+        ]);
+
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 1,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 2,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 3,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 4,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 5,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
+            'menu_id' => 6,
+            'is_allowed' => true
+        ]);
+        UserMenuAccess::create([
+            'user_role_id' => 4,
             'menu_id' => 7,
             'is_allowed' => true
         ]);
