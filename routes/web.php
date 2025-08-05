13610b6431600ae00a279e8a5ddb422d9444852d
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-05 09:40:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/storage/{path}', function ($path) {
+    $fullPath = storage_path("app/public/{$path}");
+
+    if (!file_exists($fullPath)) {
+        abort(404);
+    }
+
+    $file = File::get($fullPath);
+    $mimeType = File::mimeType($fullPath);
+
+    return response($file, 200)
+        ->header('Content-Type', $mimeType)
+        ->header('Access-Control-Allow-Origin', '*');
+})->where('path', '.*');
 
 Route::get('/', function () {
     return view('welcome');
