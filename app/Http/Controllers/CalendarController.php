d75743b9531bf3179a6cb19952557f953a7d4dec
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-07 08:41:15 +0700

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -13,11 +13,13 @@ class CalendarController extends Controller
     //
     public function getCalendar(Request $request)
     {
-        $query = FollowupMonitoring::query();
+        $query = FollowupMonitoring::query()->whereBetween(FollowupMonitoring::raw('DAY(followup_date)'), [$request->startDay, $request->endDay]);
 
         // Filter by tanggal
-        if ($request) {
-            $query->whereBetween(FollowupMonitoring::raw('DAY(followup_date)'), [$request->startDay, $request->endDay]);
+        if (auth()->user->hasRole('Sales')) {
+            $query->where('sales_id', auth()->user()->id);
+        } else if (isset($request->sales_id)) {
+            $query->where('sales_id', $request->sales_id);
         }
 
         $result = $query->get();
