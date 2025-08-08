2a30e4398e289106ee36bcfa3fcef054e3d5fdeb
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-08 09:21:38 +0700

    Update FollowupMonitoring.php

--- app/Models/FollowupMonitoring.php
+++ app/Models/FollowupMonitoring.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class FollowupMonitoring extends Model
 {
-    //
     protected $guarded = ['id'];
+
+    public function konsumen() {
+        return $this->belongsTo(Konsumen::class);
+    }
+    
+    public function prospek() {
+        return $this->belongsTo(Prospek::class);
+    }
 }
