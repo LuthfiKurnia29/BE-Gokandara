a043b2894b579e1213e54421b36c783c26c36dec
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-14 10:41:37 +0700

    Update NotifikasiController.php

--- app/Http/Controllers/NotifikasiController.php
+++ app/Http/Controllers/NotifikasiController.php
@@ -26,10 +26,43 @@ class NotifikasiController extends Controller
         return response()->json($data);
     }
 
+    public function indexUnread(Request $request)
+    {
+        $per = $request->per ?? 10;
+        $page = $request->page ?? 1;
+        $search = $request->search;
+
+        $data = Notifikasi::with('konsumen', 'chatting')
+            ->where('penerima_id', auth()->user()->id)
+            ->where('is_read', false)
+            ->where(function ($query) use ($search) {
+                if ($search) {
+                    $query->where('jenis_notifikasi', 'like', "%$search%");
+                }
+            })
+            ->orderBy('id', 'desc')
+            ->paginate($per);
+
+        return response()->json($data);
+    }
+
     public function read($id)
     {
         $notifikasi = Notifikasi::findOrFail($id);
         $notifikasi->update(['is_read' => true]);
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Notifikasi marked as read',
+            ],
+            200,
+        );
+    }
+
+    public function readAll($id)
+    {
+        $notifikasi = Notifikasi::where('penerima_id', auth()->user()->id)->update(['is_read' => true]);
 
         return response()->json(
             [
