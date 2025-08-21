e99da5e14e52f918e404c21c4ffd44ed02a1ec70
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-21 13:15:46 +0700

    Update Transaksi.php

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     protected $guarded = ['id'];
+    protected $with = ['skemaPembayaran'];
 
     public function konsumen()
     {
