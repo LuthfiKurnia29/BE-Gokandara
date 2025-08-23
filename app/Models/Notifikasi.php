907a3977414e5bf73bc57f39574ede3acd339372
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-08-23 09:56:07 +0700

    Update Notifikasi.php

--- app/Models/Notifikasi.php
+++ app/Models/Notifikasi.php
@@ -28,4 +28,14 @@ class Notifikasi extends Model
     {
         return $this->belongsTo(User::class, 'penerima_id');
     }
+    
+    public function target()
+    {
+        return $this->belongsTo(Target::class, 'target_id');
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
