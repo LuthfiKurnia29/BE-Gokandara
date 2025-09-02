5077e3a2e5a66314d0097eec8e9c70c965486dd4
wahyall <wahyall278@gmail.com>
2025-09-02 11:07:46 +0700

    Fix: file chat

--- app/Models/Chatting.php
+++ app/Models/Chatting.php
@@ -5,29 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Chatting extends Model
-{
+class Chatting extends Model {
     use SoftDeletes;
 
     protected $guarded = ['id'];
     protected $appends = ['file_url'];
 
     public function getFileUrlAttribute() {
+        if (!$this->file) {
+            return null;
+        }
         return asset('storage/' . $this->file);
     }
 
-    public function pengirim()
-    {
+    public function pengirim() {
         return $this->belongsTo(User::class, 'user_pengirim_id');
     }
 
-    public function penerima()
-    {
+    public function penerima() {
         return $this->belongsTo(User::class, 'user_penerima_id');
     }
 
-    public function notifikasi()
-    {
+    public function notifikasi() {
         return $this->hasMany(Notifikasi::class, 'chat_id');
     }
 }
