c59f73bf0c0ee6cfce9e99d70df6e00a219d8756
wahyall <wahyall278@gmail.com>
2025-09-12 09:08:01 +0700

    Fix: migration

--- database/migrations/2025_09_12_012148_create_properti_bloks_table.php
+++ database/migrations/2025_09_12_012148_create_properti_bloks_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('properti_bloks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('properti_id')->reference('propertis')->onDelete('cascade');
+            $table->foreignId('blok_id')->reference('bloks')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -20,8 +20,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('properti_bloks');
     }
 };
