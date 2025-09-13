62cb1bab1cf8c848e8cf871b66a868fb8cc7719e
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2025-09-13 22:17:37 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function __construct() {
+        ini_set('post_max_size', '124M');
+        ini_set('upload_max_filesize', '124M');
+    }
 }
